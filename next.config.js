7d962f134171ac5cf1836ed45cba0096403fcb60
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2024-09-09 21:28:29 -0500

    add cache tags

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/_forms/:site/:slug',
+        headers: [
+          {
+            key: 'Netlify-Cache-Tag',
+            value: ':site\\::slug' // Matched parameters can be used in the value
+          },
+          {
+            key: 'Cache-Tag',
+            value: ':site\\::slug' // Matched parameters can be used in the value
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
