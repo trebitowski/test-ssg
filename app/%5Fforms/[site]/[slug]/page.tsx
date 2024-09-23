8fcfab515ffe29229594b1aef7f51f0e8d2093c0
trebitowski <tyler.trebitowski@gmail.com>
2024-09-23 01:11:54 -0500

    simple page

--- app/%5Fforms/[site]/[slug]/page.tsx
+++ app/%5Fforms/[site]/[slug]/page.tsx
@@ -21,48 +21,7 @@ export const viewport: Viewport = {
   themeColor: '#000000'
 };
 
-const default_favicon = '/favicon.ico';
-const default_description = 'The most powerful no-code forms & workflows';
-const default_meta_image = 'https://form.trebitowski.com/featheryMetaImage.png';
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const result = await fetchMetadata(params.slug, params.site);
-
-  return {
-    title: result.seoTitle,
-    description: result.seoDescription || default_description,
-    icons: [
-      { rel: 'icon', url: result.favicon || default_favicon },
-      { rel: 'apple-touch-icon', url: '/logo.png' }
-    ],
-    openGraph: {
-      images: [
-        {
-          url: result.seoMetaImage || default_meta_image,
-          width: 1200,
-          height: 630
-        }
-      ]
-    },
-    manifest: '/manifest.json'
-  };
-}
-
 export default async function Page({ params }: Props) {
-  const site = params.site.replaceAll('trebitowski.com', 'feathery.io');
-  const slug = params.slug;
-
-  const redirectData = checkForHostRedirect(site);
-  console.log('Redirect Data:', redirectData);
-  if (redirectData) {
-    redirect(redirectData.redirect);
-  }
-
-  const result = await fetchMetadata(slug, site);
-  console.log('Result:', result);
-  if (result.redirect) {
-    redirect(result.redirect);
-  }
-
   console.log('Building Page', {
     slug: params.slug,
     site: params.site
@@ -70,7 +29,7 @@ export default async function Page({ params }: Props) {
 
   return (
     <Suspense>
-      <FeatheryFormPage slug={slug} site={site} {...(result as any)} />
+      <p>{`slug=${params.slug} site=${params.site}`}</p>
     </Suspense>
   );
 }
